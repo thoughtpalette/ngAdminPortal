4ae6b7e34440e83ccf97f37efa586bdcf19b820c
Devon Maloney <devon.maloney@vokalinteractive.com>
2014-11-13 15:00:17 -0600

    Move auth to $routeChangeStart event instead of listed in every resolve.

--- source/scripts/project/app.js
+++ source/scripts/project/app.js
@@ -3,79 +3,67 @@
 /* App Configuration */
 
 var angMod = angular.module( "vokal", [
-	"ngRoute",
-	"ngTouch",
-	"ngSanitize",
-	"vokal.filters",
-	"vokal.directives",
-	"angular-table",
-	"ngDialog",
-	"ui.mask",
-	"LocalStorageModule",
-	"infinite-scroll"
+    "ngRoute",
+    "ngTouch",
+    "ngSanitize",
+    "vokal.filters",
+    "vokal.directives",
+    "angular-table",
+    "ngDialog",
+    "ui.mask",
+    "LocalStorageModule",
+    "infinite-scroll"
 ] );
 
-angMod.run( function ( $rootScope, $location, Session, $http )
-{
+angMod.run( [ "$rootScope", "$location", "Session",
+    function ( $rootScope, $location, Session )
+    {
+        $rootScope.$on( "$routeChangeStart", function ( e, next )
+        {
+            next.resolve = angular.extend( next.resolve || {}, {
+                User: Session.routeAuth
+            } );
+        } );
+        $rootScope.$on( "$routeChangeSuccess", function ( e, data )
+        {
+            // Set body class based on controller
+            $rootScope.controller = data.$$route && data.$$route.controller;
 
-   $rootScope.$on( "$routeChangeSuccess", function ( ev, data )
-   {
-   		// Set body class based on controller
-		if ( data.$$route && data.$$route.controller )
-		{
-			$rootScope.controller = data.$$route.controller;
-		}
-
-		if( Session.get.authToken() && Session.get.user() && $location.path() === "/login" ) {
-			$location.path( "/list" );
-		}
-   });
-
-});
+            if( Session.get.authToken() && Session.get.user() && $location.path() === "/login" ) {
+                $location.path( "/list" );
+            }
+       } );
+    } 
+] );
 
 angMod.config( [ "$routeProvider", "$locationProvider", "$sceDelegateProvider",
+    function ( $routeProvider, $locationProvider, $sceDelegateProvider )
+    {
+        $routeProvider
+            .when( "/login", {
+                templateUrl: STATIC_PATH + "build/templates/login.html",
+                controller: "LoginCtrl"
+            } )
+            .when( "/list", {
+                templateUrl: STATIC_PATH + "build/templates/list.html",
+                controller: "ListCtrl",
+                active: "list"
+            } )
+            .when( "/infinite-list", {
+                templateUrl: STATIC_PATH + "build/templates/infinite-list.html",
+                controller: "InfiniteListCtrl",
+                active: "infinite-list"
+            } )
+            .when( "/logout", {
+                template: "",
+                controller: "LogoutCtrl"
+            } )
+            .otherwise( { redirectTo: "/login" } );
 
-	function ( $routeProvider, $locationProvider, $sceDelegateProvider )
-	{
+        $locationProvider.html5Mode( true ).hashPrefix( "!" );
 
-		var requireUser = { User: [ "$location", "$rootScope", "$q", "API", "Session",
-			function ( $location, $rootScope, $q, API, Session )
-			{
-				var deferred = $q.defer();
-
-					if( Session.get.authToken() )
-					{
-						if( Session.get.user() )
-						{
-							deferred.resolve( $rootScope.user );
-						}
-						else
-						{
-							$location.path( "/login" );
-						}
-					}
-					else
-					{
-						deferred.reject( "No user token" );
-						$location.path( "/login" );
-					}
-
-				return deferred.promise;
-
-			} ] };
-
-		$routeProvider.when( "/login", { templateUrl: STATIC_PATH + "build/templates/login.html", controller: "LoginCtrl" } )
-			.when( "/list", { templateUrl: STATIC_PATH + "build/templates/list.html", controller: "ListCtrl", resolve: requireUser, active: "list" } )
-			.when( "/infinite-list", { templateUrl: STATIC_PATH + "build/templates/infinite-list.html", controller: "InfiniteListCtrl", resolve: requireUser, active: "infinite-list" } )
-			.when( "/logout", { template: " ", controller: "LogoutCtrl"})
-			.otherwise( { redirectTo: "/login" } );
-
-		$locationProvider.html5Mode( true ).hashPrefix( "!" );
-
-		$sceDelegateProvider.resourceUrlWhitelist(
-			[ "self", "http://*.s3.amazonaws.com/**", "https://*.s3.amazonaws.com/**" ]
-		);
-
-	}
-
+        $sceDelegateProvider.resourceUrlWhitelist(
+            [ "self", "http://*.s3.amazonaws.com/**", "https://*.s3.amazonaws.com/**" ]
+        );
+    }
 ] );
--- source/scripts/project/services/session-srvc.js
+++ source/scripts/project/services/session-srvc.js
@@ -1,5 +1,5 @@
-angMod.service( "Session", ["localStorageService", "$rootScope", "$location", "$http",
-    function ( localStorageService, $rootScope, $location, $http )
+angMod.service( "Session", ["localStorageService", "$rootScope", "$location", "$http", "$q",
+    function ( localStorageService, $rootScope, $location, $http, $q )
     {
 
         this.create = function ( user )
@@ -34,6 +34,30 @@ angMod.service( "Session", ["localStorageService", "$rootScope", "$location", "$
                 }
             }
 
+        };
+
+        function isAuth ()
+        {
+            return localStorageService.get( "authToken" ) && localStorageService.get( "user.role" );
+        }
+        this.routeAuth = function ()
+        {
+            var deferred = $q.defer();
+            var publicPaths = [ "/login", "/logout" ];
+            if ( publicPaths.indexOf( $location.path() ) !== -1 )
+            {
+                deferred.resolve( null );
+            }
+            else if( isAuth() )
+            {
+                deferred.resolve( $rootScope.user );
+            }
+            else
+            {
+                deferred.reject( "No user token" );
+                $location.path( "/login" );
+            }
+            return deferred.promise;
         };
 
         this.destroy = function ()
