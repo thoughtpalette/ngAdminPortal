636fd19f7752f807cb2b753ac1740c272e9c3e09
Devon Maloney <devon.maloney@vokalinteractive.com>
2014-11-20 11:49:52 -0600

    Convert tabs to spaces.

--- protractor_config.js
+++ protractor_config.js
@@ -6,34 +6,34 @@ exports.config = {
     // Leave seleniumServerJar commented out to default to the correct version url
     // seleniumServerJar: "node_modules/protractor/selenium/selenium-server-standalone",
 
-	// Capabilities to be passed to the webdriver instance.
-	capabilities: {
-		"browserName": "chrome"
-	},
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        "browserName": "chrome"
+    },
 
     // Prevents a full selenium start-up, much quicker, but only works with Chrome and Firefox
     directConnect: true,
 
-	baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
 
-	rootElement: 'html',
+    rootElement: 'html',
 
-	// Spec patterns are relative to the current working directly when protractor is called.
-	specs: [
-		"tests/login-test.js",
-		"tests/list-test.js"
-	],
+    // Spec patterns are relative to the current working directly when protractor is called.
+    specs: [
+        "tests/login-test.js",
+        "tests/list-test.js"
+    ],
 
-	// Options to be passed to Jasmine-node.
-	jasmineNodeOpts: {
-	    // If true, display spec names.
-	    isVerbose: true,
-	    // If true, print colors to the terminal.
-	    showColors: true,
-	    // If true, include stack traces in failures.
-	    includeStackTrace: true,
-	    // Default time to wait in ms before a test fails.
-	    defaultTimeoutInterval: 30000
-	}
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        // If true, display spec names.
+        isVerbose: true,
+        // If true, print colors to the terminal.
+        showColors: true,
+        // If true, include stack traces in failures.
+        includeStackTrace: true,
+        // Default time to wait in ms before a test fails.
+        defaultTimeoutInterval: 30000
+    }
 
 };
